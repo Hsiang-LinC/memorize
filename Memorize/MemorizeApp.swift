be3da638e19848c08f5add917003315753f8fb3d
Hsiang-LinC <hsiang-lin.chan@rwth-aachen.de>
2021-12-28 11:33:44 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Danny Chan on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
