f0deb707938f17cdb86289343440ab161e7b9747
Hsiang-LinC <hsiang-lin.chan@rwth-aachen.de>
2021-12-29 01:01:24 +0100

    add something

--- Memorize/MemorizedView.swift
+++ Memorize/MemorizedView.swift
@@ -13,18 +13,45 @@ struct MemorizedView: View {
     // Declearing that our struct MemorizedView behave like View (and get all the functionality of View
     // And to behave like a View, this need the variable body showed below
     
+    var emojis = ["🚌", "🚎", "🚒", "🚚"]
+    
     // In functional programming (at least in SwiftUI here), the varibale like this is not stored
     // in memory. Instead it will run the function of it and return a value that matches the type
-    // (here Text is a View struct)
+    // (here Text is a View struct).
     // why not just set body: Text?
-    // Since it might get more complicated when your code grows
-    
+    // Since it might get more complicated when your code grows.
     var body: some View { // To build a Lego house, you need "some" Lege furniture :)
-        /*Text("Hello, Swift!") // return Text, which "return" is saved to look cleaner
-            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/) // this padding function return something like a View, but definitely not Text
-            .foregroundColor(color: Color.orange) // or just (.orange), Swift is smart enough
-         */
-        RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 3).padding(.horizontal)
+        HStack { // View combiner
+            ForEach(emojis[0..<4], id: \.self) {emoji in
+                CardView(content: emoji)
+            }
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red) // Defualt color of the whole stack, can be override respecitvely.
+    }
+}
+
+
+// We want more small views rather than few large views.
+struct CardView: View {
+    @State var isFaceUp = false // @State ???
+    var content: String
+    
+    var body: some View {
+        ZStack { // This stack views together, the return type is more than ZStack btw.
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3).foregroundColor(Color.orange)
+                Text(content).font(.largeTitle) // return Text, which "return" is saved to look cleaner.
+            }
+            else {
+                shape.fill().foregroundColor(.blue)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
@@ -47,5 +74,6 @@ struct MemorizedView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MemorizedView()
+            .preferredColorScheme(.dark)
     }
 }
